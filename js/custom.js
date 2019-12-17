9fe615c1ea6a5e167348ba31ecb9a71d013b4e8a
adunne8 <adunne08@gmail.com>
2019-12-17 21:03:04 +0000

    Click Fix - #4
    Refactored code for pizza and changed naming conventions from aa_bb to aaBb

--- js/custom.js
+++ js/custom.js
@@ -1,18 +1,17 @@
-let food = 0;
+// DECLARATIONS
+let pizzas = 0;
+
+const pizzaButton = document.getElementById('pizza__button');
 
 
 function generate_food_fnc(){
-    console.log("Generate Food"); 
-    food++;
-    console.log(food);
+    console.log("Generate Pizza"); 
+    pizzas++;
+    console.log(pizzas);
 }
 
 function update_resource_display_fnc(){
 
 }
 
-
-
-const generate_food = document.getElementById('generate-food');
-
-generate_food.addEventListener("click", generate_food_fnc);
+pizzaButton.addEventListener("click", generate_food_fnc);
